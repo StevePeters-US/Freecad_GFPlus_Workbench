0e2550acc263e0cbe62ae25ccf048fb25f33610a
Steve <spete008@gmail.com>
2025-01-23 21:17:34 -0800

    Repaired gf base

--- GridifintyPlusWorkbench/freecad/gridfinity_plus_workbench/CreateBaseplate.py
+++ GridifintyPlusWorkbench/freecad/gridfinity_plus_workbench/CreateBaseplate.py
@@ -1,40 +1,42 @@
 import FreeCAD, FreeCADGui, Part, Sketcher
 from PySide import QtGui, QtCore
+import math
 import os
 from freecad.gridfinity_plus_workbench.CreateBaseplateTaskPanel import BaseplateTaskPanel
-
 
 ICONPATH = os.path.join(os.path.dirname(__file__), "resources//icons//")
 
 class CreateBaseplate:
+
     def __init__(self):
         self.taskPanel = None
-        self.baseplate_doc_path = None
 
     def GetResources(self):
-        return {
-            "Pixmap": str(ICONPATH + "layout-grid-line.svg"),
-            "Accel": "Shift+S",
-            "MenuText": "Create GF+ Baseplate",
-            "ToolTip": "Create GF+ Baseplate"
-        }
+        return {"Pixmap"  : str(ICONPATH + "layout-grid-line.svg"),  
+                "Accel"   : "Shift+S",  # a default shortcut (optional)
+                "MenuText": "Create GF+ Baseplate",
+                "ToolTip" : "Create GF+ Baseplate"}
 
     def Activated(self):
         self.showTaskPanel()
 
     def IsActive(self):
+        """Define when the command is active (optional)"""
         return True
 
     def showTaskPanel(self):
         self.taskPanel = BaseplateTaskPanel(self)
         FreeCADGui.Control.showDialog(self.taskPanel)
 
-  
     def CreateBaseplate(self, NumX, NumY):
+        # Get the active document
         doc = FreeCAD.ActiveDocument
-        if not doc:
-            doc = FreeCAD.newDocument("Unnamed")
         
+        # Create a new document if none exists
+        if doc is None:
+            doc = FreeCAD.newDocument("Unnamed")
+                
+        # Delete all previously created bodies
         for obj in doc.Objects:
             if obj.Name.startswith("ImportedBaseplate"):
                 doc.removeObject(obj.Name)
@@ -44,62 +46,40 @@ class CreateBaseplate:
         template_dir = os.path.dirname(script_path) + "/resources"
         
         baseplate_doc_file_path = os.path.join(template_dir, "Gridfinity+Base_Template.FCStd")
+
         baseplate_doc = FreeCAD.open(baseplate_doc_file_path)
-
-        if not baseplate_doc:
-            print("Failed to set up the baseplate document.")
-            return
         
-        feature_name = 'Body'
+        # Access and modify the spreadsheet in the baseplate document
+        spreadsheet = baseplate_doc.getObject("Spreadsheet")
+        if spreadsheet:
+            # Adjust parameters as needed
+            spreadsheet.set("SizeX", str(NumX))
+            spreadsheet.set("SizeY", str(NumY))
+            baseplate_doc.recompute()
+
+        # Replace 'YourFeatureName' with the actual name of the feature you're looking for
+        feature_name = 'Cut001'
         baseplate_feature = baseplate_doc.getObject(feature_name)
-        
-        if not baseplate_feature:
-            print(f"No feature named '{feature_name}' found in the baseplate document.")
+
+        if baseplate_feature is None:
+            print(f"No feature named {feature_name} found in the baseplate document.")
             return
-
-  
         
-        shapes_for_fusion = []
+            
+            # Add the new baseplate body
+        new_baseplate_body = doc.addObject('Part::Feature', 'ImportedBaseplate')
+        new_baseplate_body.Shape = baseplate_feature.Shape
 
-        for x in range(NumX):
-            for y in range(NumY):
-                new_body = doc.addObject('Part::Feature', f'ImportedBaseplate_{x}_{y}')
-                print(f"New body '{new_body.Name}' created at position ({x}, {y}).")
-
-                new_body.Shape = baseplate_feature.Shape
-                
-                center = FreeCAD.Vector(-20.75, 0, 0) 
-                pos = new_body.Placement.Base + FreeCAD.Vector(x * 42, y * 42, 0) - center
-                #rot = new_body.Placement.Rotation.multiply(FreeCAD.Rotation(FreeCAD.Vector(0, 1, 0), slice * 90))
-                # Apply the new placement with the rotation and the correct center
-                new_body.Placement.Base = pos
-                new_body.Placement = FreeCAD.Placement(pos + center, FreeCAD.Rotation(FreeCAD.Vector(0, 0, 0), center))
-
-                view_object = new_body.ViewObject
-                view_object.ShapeColor = (0.25, 0.25, 0.25)
-
-                shapes_for_fusion.append(new_body.Shape)
-        
-       
-
-        # compound = Part.makeCompound(shapes_for_fusion)
-
-        # fusion = doc.addObject('Part::Feature', f'Baseplate_{NumX}_{NumY}')
-        # fusion.Shape = compound
-
-        # view_object = fusion.ViewObject
-        # view_object.ShapeColor = (0.0, 0.88, 0.11)
-
+        # Change the color of the new baseplate body
+        view_object = new_baseplate_body.ViewObject
+        view_object.ShapeColor = (0.25, 0.25, 0.25)  
 
         doc.recompute()
         
-        try:
-            FreeCAD.closeDocument(baseplate_doc.Name)
-        except Exception as e:
-            print(f"Error closing the baseplate document: {e}")
+        # Close the stored documents after copying
+        FreeCAD.closeDocument(baseplate_doc.Name)
+
+
+
 
 FreeCADGui.addCommand("CreateBaseplateCommand", CreateBaseplate())
-
-
-
-
